356e5e6ee7e6802296285294f5b1c8cc7ae50615
pieconsume <130113511+pieconsume@users.noreply.github.com>
2024-02-28 21:51:44 -0500

    More client/server stuff

--- Host.js
+++ Host.js
@@ -1,4 +1,11 @@
 const { WebSocketServer } = require("ws");
 const server = new WebSocketServer({ port: 6075 });
-server.on("connection", ws => { console.log("new connection"); });
+let client1, client2;
+let client1white = null, gamestarted = false, whiteturn = true;
+server.on("connection", client => {
+ if      (client1 == null) { console.log("client1 connected"); client1 = client; client1.on("message", (msg) => { if (client2 != null) { client2.send(msg); } }); client1.on("close", () => { client1 = null; }); }
+ else if (client2 == null) { console.log("client2 connected"); client2 = client; client2.on("message", (msg) => { if (client1 != null) { client1.send(msg); } }); client2.on("close", () => { client2 = null; }); }
+ else                      { console.log("blocked extra client"); }
+ if (client1 != null && client2 != null) { gamestarted = true; }
+ });
 console.log("Hosting");
